840ebfb1342c1a4059c9d48cdd7cace70714ae75
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-27 12:12:19 +0300

    Add get_count_for_letter func

--- wikiparsing.py
+++ wikiparsing.py
@@ -7,27 +7,40 @@ a = ord("а")
 LETTERS = {chr(i).upper(): 0 for i in range(a, a + 32)}
 
 
-for letter in LETTERS:
+def get_count_for_letter(letter):
     params = {"title": "Категория:Животные_по_алфавиту", "from": letter}
+    count = 0
 
     while True:
-        response = requests.get(URL, params=params)
+        session = requests.Session()
+        response = session.get(URL, params=params)
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred for the letter '{letter}': {http_err}")
+            return
+
         soup = BeautifulSoup(response.text, "html.parser")
         animal_block = soup.find("div", class_="mw-category-group")
         animals = animal_block.text.split("\n")
 
         if animals[0] != letter:
-            break
+            return count
 
-        LETTERS[letter] += len(animals) - 1
+        count += len(animals) - 1
+
+        if len(animals) < 201:
+            return count
 
         params = {
             "title": "Категория:Животные_по_алфавиту",
             "pagefrom": animals[-1],
         }
 
-        if len(animals) < 201:
-            break
 
-for key, value in LETTERS.items():
-    print(f"{key}: {value}")
+if __name__ == "__main__":
+    for letter in LETTERS:
+        count = get_count_for_letter(letter)
+        LETTERS[letter] = count
+        print(f"{letter}: {count}")
